19e9d1cbf3889a15d3b76d9e13329ce1e04a807d
pg929348-hash <pg929348@gmail.com>
2026-09-13 20:35:44 +0530

    Add pin code validation and character indexing

---
+++ Python indexing.py
@@ -0,0 +1,7 @@
+while True:
+    b= int( input(" enter your pin code:-"))
+    if b==89076:
+        print (" now you can move")
+        hel=input("enter your name:-")
+        for index,char in enumerate(hel):
+                print(f"index{index}='{char}'")
